5880e2f77ceaf789d54c244c555bde8578fda14f
Abdel Raoof <olakara@gmail.com>
2015-01-03 11:14:32 +0400

    Attempt to use popover

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-angular.module('MyApp', ["treeControl"])
-	.controller('AppCtrl', function($scope) {
+(function () {
+    'use strict';
+    
+    var myApp = angular.module('MyApp', ["treeControl"]);
+    
+	myApp.controller('AppCtrl', function($scope) {
 
+        $("#example").popover(); 
+    
 		$scope.heading = "Tree POC";
 
 		$scope.selectedItem = "";
@@ -9,7 +15,7 @@ angular.module('MyApp', ["treeControl"])
 			nodeChildren: "subitems"
 		};
 
-		$scope.data = [{id:0,label:"أرض",subitems:[
+		$scope.data = [{id: 0, label: "أرض", subitems: [
 	{
 		id: 1,
 		label: "Africa",
@@ -24,7 +30,7 @@ angular.module('MyApp', ["treeControl"])
 		}, {
 			id: 13,	label: "Libya",	subitems: []
 		}, {
-			id: 14,	label: "Morocco",subitems: []
+			id: 14,	label: "Morocco", subitems: []
 		}, {
 			id: 15,	label: "Sudan",	subitems: []
 		}, {
@@ -34,38 +40,38 @@ angular.module('MyApp', ["treeControl"])
 		id: 2,
 		label: "Europe",
 		subitems: [{
-			id: 21,label: "Albania",subitems: []
+			id: 21, label: "Albania", subitems: []
 		}, {
-			id: 22,label: "Belgium",subitems: []
+			id: 22, label: "Belgium", subitems: []
 		}, {
-			id: 23,label: "Finland",subitems: []
+			id: 23, label: "Finland", subitems: []
 		}, {
-			id: 24,label: "Denmark",subitems: []
+			id: 24, label: "Denmark", subitems: []
 		}, {
-			id: 25,	label: "Germany",subitems: []
+			id: 25,	label: "Germany", subitems: []
 		}, {
-			id: 26,label: "Greece",subitems: []
+			id: 26, label: "Greece", subitems: []
 		}, {
-			id: 27,label: "Latvia",subitems: []
+			id: 27, label: "Latvia", subitems: []
 		}]
 	}, {
 		id: 3,
 		label: "Asia",
 		subitems: [{
-			id: 31,label: "Maldives",subitems: []
+			id: 31, label: "Maldives", subitems: []
 		}, {
-			id: 32,	label: "United Arab Emirates",subitems: [
-		{id:321,label: "Fujairah",subitems:[]}
+			id: 32,	label: "United Arab Emirates", subitems: [
+		{id: 321, label: "Fujairah", subitems:[]}
 		]
 	}]
 }, {
-	id: 4,label: "America",	subitems: [{
-		id: 41,label: "Canada",subitems: []
-	}, ]
+	id: 4, label: "America",	subitems: [{
+		id: 41, label: "Canada", subitems: []
+	}, ] 
 }, {
-	id: 5,label: "Antarctica",subitems: []
+	id: 5, label: "Antarctica", subitems: []
 }, {
-	id: 6,label: "Australia",	subitems: []
+	id: 6, label: "Australia",	subitems: []
 }
 		]}];
 
@@ -75,3 +81,5 @@ angular.module('MyApp', ["treeControl"])
 		};
 
 	});
+
+})();
